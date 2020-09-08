7ef7b5ba4a6e08e83c55d71faa610c9858b5979f
Sooraj Ep <epsooraj4@gmail.com>
2020-09-08 23:58:31 +0530

    Changing to DateTime, ordering with date - descending

--- diary/models.py
+++ diary/models.py
@@ -13,7 +13,7 @@ class Diary(models.Model):
     '''
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    date = models.DateField(default=datetime.date.today)
+    date = models.DateTimeField(auto_now_add=True)
     heading = models.TextField(null=True, blank=True)
     text = models.TextField(null=True, blank=True)
 
@@ -22,3 +22,4 @@ class Diary(models.Model):
 
     class Meta:
         verbose_name_plural = "diaries"
+        ordering = ('-date',)
