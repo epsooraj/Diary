9b464e11805d411a2ff09c53dbb18037b7ba87c8
Sooraj Ep <epsooraj4@gmail.com>
2020-09-08 23:57:29 +0530

    redirecting if already loggedin

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@
 '''
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 
 class LoginView(TemplateView):
@@ -11,4 +12,6 @@ class LoginView(TemplateView):
     '''
 
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/diary/")
         return render(request, "accounts/login.html", {})
