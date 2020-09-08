80ff73d3ed5c0dc0f825d988ba2cdb3a00b91e58
Sooraj Ep <epsooraj4@gmail.com>
2020-09-09 00:01:27 +0530

    Static DIRS, Time Zone IST

--- diaryapp/settings.py
+++ diaryapp/settings.py
@@ -133,7 +133,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
@@ -144,5 +144,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_URL = '/static/'
