87fec47e0700a460eeba9bc4e1075c842a85a41f
Sooraj Ep <epsooraj4@gmail.com>
2020-09-08 23:59:38 +0530

    Page view, edit/delete (action) urls

--- diary/urls.py
+++ diary/urls.py
@@ -5,5 +5,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:postid>/', views.DiaryPage.as_view(), name="diaryPage"),
+    path('<int:postid>/<slug:action>/',
+         views.DiaryPage.as_view(), name="diaryPageEdit"),
     path('', views.DiaryView.as_view(), name="diary"),
 ]
