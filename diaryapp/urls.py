113390f03476ed6e96616f29fa01696dab4be284
Sooraj Ep <epsooraj4@gmail.com>
2020-09-09 19:55:55 +0530

    Adding redirect on /

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -3,11 +3,12 @@
 '''
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
     path('diary/', include("diary.urls")),
-    path('', include("diary.urls")),
+    path('', RedirectView.as_view(url="/diary/")),
 ]
