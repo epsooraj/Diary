ed48661fbed0d24f91b591397df40c1a06539339
Sooraj Ep <epsooraj4@gmail.com>
2020-09-09 16:21:01 +0530

    padding and border radius

--- diary/forms.py
+++ diary/forms.py
@@ -9,6 +9,6 @@ class DiaryForm(forms.Form):
     @docstring
     '''
     heading = forms.CharField(label="Heading", widget=forms.TextInput(
-        attrs={'class': 'mb-4 w-100 display-4'}))
+        attrs={'class': 'mb-4 w-100 display-4 p-2 rounded-lg'}))
     body = forms.CharField(label="Diary", widget=forms.Textarea(
-        attrs={'class': 'mb-4 w-100 lead'}))
+        attrs={'class': 'mb-4 w-100 lead p-2 rounded-lg'}))
