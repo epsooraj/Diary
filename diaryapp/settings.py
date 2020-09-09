e155b221f29a5158b1b067e669d88c8cb22f8a22
Sooraj Ep <epsooraj4@gmail.com>
2020-09-09 14:37:13 +0530

    SITE_ID to env

--- diaryapp/settings.py
+++ diaryapp/settings.py
@@ -74,7 +74,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 2
+SITE_ID = os.environ['SITE_ID']
 LOGIN_REDIRECT_URL = '/'
 
 SOCIALACCOUNT_PROVIDERS = {
