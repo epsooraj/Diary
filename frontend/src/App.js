52641b632a480d6b3edc5dce3a6f003aae7ce115
Sooraj Ep <epsooraj4@gmail.com>
2020-09-15 13:57:29 +0530

    Test pages and views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./views/home";
+import Login from "./components/login";
+import Signup from "./components/signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login/" component={Login} />
+        <Route path="/signup/" component={Signup} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
