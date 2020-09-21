a9a08ef0e5c54476d845fe8b616da45d92d8ad1f
Sooraj Ep <epsooraj4@gmail.com>
2020-09-21 23:22:58 +0530

    Patches

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -7,8 +7,10 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('auth/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
+
     path('diary/', include("diary.urls")),
     path('api/', include("diary.urls")),
     path('', RedirectView.as_view(url="/diary/")),
