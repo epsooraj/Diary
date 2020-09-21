75b4eff30d8a0d8b4da628e3874f7b383ad671f8
Sooraj Ep <epsooraj4@gmail.com>
2020-09-21 23:22:34 +0530

    Comments

--- diary/urls.py
+++ diary/urls.py
@@ -7,10 +7,11 @@ from . import views
 
 urlpatterns = [
 
+    # @route /api/
     path('diary/', views.DiaryPagesList.as_view(), name="diarypagesapi"),
-
     path('diary/<int:id>', views.DiaryPageView.as_view(), name="diarypageapi"),
 
+    # @route /diary/
     path('<int:postid>/', views.DiaryPage.as_view(), name="diaryPage"),
     path('<int:postid>/<slug:action>/',
          views.DiaryPage.as_view(), name="diaryPageEdit"),
