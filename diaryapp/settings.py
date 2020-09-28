f982655b7124813aaec537828aa2ca889f04fb0a
Sooraj Ep <epsooraj4@gmail.com>
2020-09-29 00:57:48 +0530

    staticfiledirs

--- diaryapp/settings.py
+++ diaryapp/settings.py
@@ -227,10 +227,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+    BASE_DIR / 'frontend' / 'src' / 'assets',
+)
