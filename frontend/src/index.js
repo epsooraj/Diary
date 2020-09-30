bb314e56eabce0ad51be52e598bb8963176ac29c
Sooraj Ep <epsooraj4@gmail.com>
2020-09-30 15:52:01 +0530

    Adding regenrator-runtime import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+import "regenerator-runtime/runtime.js";
+
 import App from "./App";
 
 const container = document.getElementById("app");
