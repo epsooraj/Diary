9b7e5698dc5ec76809ab04d954720e0a915f9df3
Sooraj Ep <epsooraj4@gmail.com>
2020-10-02 20:44:57 +0530

    Changing import of css from javascript to html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 import "regenerator-runtime/runtime.js";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
 
