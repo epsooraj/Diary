051f0f886539c18e0e5adb3bea04d6c2356719db
Sooraj Ep <epsooraj4@gmail.com>
2020-10-02 19:51:01 +0530

    Changing Token name from JWT to Token

--- diaryapp/settings.py
+++ diaryapp/settings.py
@@ -100,7 +100,7 @@ SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
-    'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('Token',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
