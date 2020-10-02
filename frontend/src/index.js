83aa0128f1731c92e87a8ff176a19aba8cb2bed1
Sooraj Ep <epsooraj4@gmail.com>
2020-10-02 16:34:44 +0530

    adding bootstrap css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 import "regenerator-runtime/runtime.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
 
