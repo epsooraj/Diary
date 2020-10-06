1e33dabdd053ffd5ecea45c2a08e626375ec523e
Sooraj Ep <epsooraj4@gmail.com>
2020-10-06 16:30:54 +0530

    Unique email and username validators

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
 
-    email = serializers.EmailField(required=True)
-    username = serializers.CharField()
+    email = serializers.EmailField(required=True, validators=[
+        UniqueValidator(
+            queryset=User.objects.all(),
+            message="Email already exist"
+        )
+    ])
+    username = serializers.CharField(validators=[
+        UniqueValidator(
+            queryset=User.objects.all(),
+            message="Username already exist"
+        )
+    ])
     password = serializers.CharField(min_length=8, write_only=True)
 
     class Meta:
